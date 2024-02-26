e8f4b3e6fb2d009ef08c838b7ebe8f6ee0afe983
Daffier <63510648+0-DAyFF13R@users.noreply.github.com>
2024-02-26 15:17:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var images = document.getElementsByTagName("img");
-
-for (var i = 0; i < images.length; i++) {
-  images[i].src = "https://avatars.dzeninfra.ru/get-zen_doc/5324345/pub_6328738d6fdf0c5366a4a38a_632879b56b77c84bee0f68bc/scale_2400";
-}
+document.addEventListener("DOMContentLoaded", function() {
+  var images = document.getElementsByTagName("img");
+  
+  for (var i = 0; i < images.length; i++) {
+    images[i].src = "https://avatars.dzeninfra.ru/get-zen_doc/5324345/pub_6328738d6fdf0c5366a4a38a_632879b56b77c84bee0f68bc/scale_2400";
+  }
+});
