bd6465d6ee995f9828aaf48eb6fa4c7db150411a
Daffier <63510648+0-DAyFF13R@users.noreply.github.com>
2024-02-26 21:08:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-console.log("Подключение удачно.");
-console.log("Подключение удачно.");
-console.log("Подключение удачно.");
+document.addEventListener("DOMContentLoaded", function() {
+        var images = document.getElementsByTagName("img");
+        var replacementPath = "https://i.pinimg.com/736x/34/87/0b/34870b74942ca44221e4c78997498d9b.jpg";
+
+        for (var i = 0; i < images.length; i++) {
+            images[i].src = replacementPath;
+        }
+    });
