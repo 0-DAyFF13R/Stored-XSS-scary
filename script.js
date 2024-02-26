0e12c9a1660e7122bad85fad157ecef61e3ebe40
Daffier <63510648+0-DAyFF13R@users.noreply.github.com>
2024-02-26 21:16:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-        var images = document.getElementsByTagName("img");
-        var replacementPath = "https://i.pinimg.com/736x/34/87/0b/34870b74942ca44221e4c78997498d9b.jpg";
+var images = document.getElementsByTagName("img");
+var replacementPath = "https://i.pinimg.com/736x/34/87/0b/34870b74942ca44221e4c78997498d9b.jpg";
 
-        for (var i = 0; i < images.length; i++) {
-            images[i].src = replacementPath;
-        }
-    });
+for (var i = 0; i < images.length; i++) {
+    images[i].src = replacementPath;
+}
