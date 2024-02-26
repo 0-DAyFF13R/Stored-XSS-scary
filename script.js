a84d6b78a8d14672e4f79b7854928541c73d66d0
Daffier <63510648+0-DAyFF13R@users.noreply.github.com>
2024-02-26 20:47:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-console.log("WORKING!!!!!!!!!!!!!!!!!");
-document.addEventListener("DOMContentLoaded", function() {
-  console.log("WORKING!!!!!!!!!!!!!!!!!");
-  var images = document.getElementsByTagName("img");
-  
-  for (var i = 0; i < images.length; i++) {
-    images[i].src = "https://avatars.dzeninfra.ru/get-zen_doc/5324345/pub_6328738d6fdf0c5366a4a38a_632879b56b77c84bee0f68bc/scale_2400";
-  }
-});
+console.log("Подключение удачно.");
